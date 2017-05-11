bca72a0782672487102bbdda9aebc40361324c20
Vlad T <vladyslav.tar@gmail.com>
2017-05-11 03:44:38 +0300

    Routing
    Add routing

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+Config::set('site_name', 'Your site name');
+
+Config::set('languages', array('en', 'ua'));
+
+// Routes. Route name => method prefix
+Config::set('routes', array('default' => '', 'admin' => 'admin_'));
+
+Config::set('default_route', 'default');
+Config::set('default_language', 'en');
+Config::set('default_controller', 'pages');
+Config::set('default_action', 'index');
